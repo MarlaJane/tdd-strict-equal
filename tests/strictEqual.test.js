ff6c5a5b4c201e7fa80dae2b867a8de8d1488080
MarlaJane <61168863+MarlaJane@users.noreply.github.com>
2020-10-24 13:07:10 -0400

    cleanup post class

--- tests/strictEqual.test.js
+++ tests/strictEqual.test.js
@@ -5,28 +5,24 @@ const sEqual = require('../strictEqual')
 
 describe('StrictEqual', () => {
 	describe('sEqual', () => {
-		it('returns true if both parameters are equal integers', () => {
+		it('returns true when given two parameters of the same type and value', () => {
 			const answer = sEqual(1, 1)
-			expect(answer); (true)
+			expect(answer).to.equal(true)
 		})
 
-		it('returns true if both parameters are equal strings', () => {
-			const answer = sEqual('a', 'a')
-			expect(answer); (true)
+		it('returns false when given two parameters of the different type and same value', () => {
+			const answer = sEqual(1, '1')
+			expect(answer).to.equal(false)
 		})
 
-		it('returns false if both parameters are not equal integers', () => {
+		it('returns false when given two parameters of the same type and different values', () => {
 			const answer = sEqual(1, 3)
-			expect(answer); (false)
+			expect(answer).to.equal(false)
 		})
 
-		it('returns false if both parameters are not equal integers', () => {
-			const answer = sEqual('a', 'b')
-			expect(answer); (false)
-		})
-		it('returns false if both parameters are not the same dataype', () => {
-			const answer = sEqual(1, 'a')
-			expect(answer); (false)
+		it('returns false when given two parameters of different types and diffeent values', () => {
+			const answer = sEqual('a', 3)
+			expect(answer).to.equal(false)
 		})
 	})
 })
